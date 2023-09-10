826d8176c8ee5dadbaa7186d2dd4fa391d7e35d1
dawnz <dawnz1@hotmail.com>
2023-09-10 13:15:09 -0500

    added middleware with graphql setup

---
+++ jamstock_middlware/src/main/java/com/daas/jamstock_middlware/JamstockMiddlwareApplication.java
@@ -0,0 +1,13 @@
+package com.daas.jamstock_middlware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamstockMiddlwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamstockMiddlwareApplication.class, args);
+	}
+
+}
---
+++ jamstock_middlware/src/test/java/com/daas/jamstock_middlware/JamstockMiddlwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daas.jamstock_middlware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamstockMiddlwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
