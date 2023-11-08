bbec9dbde20f46682eaa27d8d6ab1ea4e153e4df
dawnz <dawnz1@hotmail.com>
2023-11-07 22:49:16 -0500

    added dummy currencies on startup

--- jamstock_middlware/src/main/java/com/daas/jamstock_middlware/JamstockMiddlwareApplication.java
+++ jamstock_middlware/src/main/java/com/daas/jamstock_middlware/JamstockMiddlwareApplication.java
@@ -1,13 +1,25 @@
 package com.daas.jamstock_middlware;
 
+import com.daas.jamstock_middlware.models.Currency;
+import com.daas.jamstock_middlware.repositories.CurrencyRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JamstockMiddlwareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JamstockMiddlwareApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner applicationRunner (CurrencyRepository currencyRepository){
+		return args -> {
+			 currencyRepository.save(new Currency(null,"JMD"));
+			 currencyRepository.save(new Currency(null,"USD"));
+		};
 	}
 
 }
